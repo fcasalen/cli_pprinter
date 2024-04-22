3c0053675ec7ebdd80d6d2503f373f61a007f2ea
fcasalen <fcasalen@gmail.com>
2024-04-22 16:45:46 -0300

    version 0.1.2: bu g in white_underline method and adjusts so it doesnt occur it again

--- cli_pprinter/colors_terminal.py
+++ cli_pprinter/colors_terminal.py
@@ -1,5 +1,8 @@
 from termcolor import cprint
 
+attrs_br = ['bold', 'reverse']
+attrs_bru = ['bold', 'reverse', 'underline']
+
 class CLIPPrinter:
     def __adjust_msg__(msg:str):
         return msg + '\n'
@@ -7,49 +10,49 @@ class CLIPPrinter:
     @staticmethod
     def cyan_underline(msg: str, **kwargs):
         'cyan, bold, underline, reverse'
-        cprint(text = CLIPPrinter.__adjust_msg__(msg), color = 'cyan', attrs = ['bold', 'underline', 'reverse'], **kwargs)
+        cprint(text = CLIPPrinter.__adjust_msg__(msg), color = 'cyan', attrs = attrs_bru, **kwargs)
 
     @staticmethod
     def cyan(msg: str, **kwargs):
         'cyan, bold, underline, reverse'
-        cprint(text = CLIPPrinter.__adjust_msg__(msg), color = 'cyan', attrs = ['bold', 'reverse'], **kwargs)
+        cprint(text = CLIPPrinter.__adjust_msg__(msg), color = 'cyan', attrs = attrs_br, **kwargs)
 
     @staticmethod
     def green(msg: str, **kwargs):
         'green, bold, reverse'
-        cprint(text = CLIPPrinter.__adjust_msg__(msg), color = 'green', attrs = ['bold', 'reverse'], **kwargs)
+        cprint(text = CLIPPrinter.__adjust_msg__(msg), color = 'green', attrs = attrs_br, **kwargs)
     
     @staticmethod
     def green_underline(msg: str, **kwargs):
         'green, bold, reverse'
-        cprint(text = CLIPPrinter.__adjust_msg__(msg), color = 'green', attrs = ['bold', 'reverse', 'underline'], **kwargs)
+        cprint(text = CLIPPrinter.__adjust_msg__(msg), color = 'green', attrs = attrs_bru, **kwargs)
 
     @staticmethod
     def red(msg: str, **kwargs):
         'red, bold, reverse'
-        cprint(text = CLIPPrinter.__adjust_msg__(msg), color = 'red', attrs = ['bold', 'reverse'], **kwargs)
+        cprint(text = CLIPPrinter.__adjust_msg__(msg), color = 'red', attrs = attrs_br, **kwargs)
 
     @staticmethod
     def red_underline(msg: str,**kwargs):
         'red, bold, underline, reverse'
-        cprint(text = CLIPPrinter.__adjust_msg__(msg), color = 'red', attrs = ['bold', 'underline', 'reverse'], **kwargs)
+        cprint(text = CLIPPrinter.__adjust_msg__(msg), color = 'red', attrs = attrs_bru, **kwargs)
 
     @staticmethod
     def yellow(msg: str, **kwargs):
         'yellow, bold, reverse'
-        cprint(text = CLIPPrinter.__adjust_msg__(msg), color = 'yellow', attrs = ['bold', 'reverse'], **kwargs)
+        cprint(text = CLIPPrinter.__adjust_msg__(msg), color = 'yellow', attrs = attrs_br, **kwargs)
 
     @staticmethod
     def yellow_underline(msg: str, **kwargs):
         'yellow, bold, reverse'
-        cprint(text = CLIPPrinter.__adjust_msg__(msg), color = 'yellow', attrs = ['bold', 'reverse', 'underline'], **kwargs)
+        cprint(text = CLIPPrinter.__adjust_msg__(msg), color = 'yellow', attrs = attrs_bru, **kwargs)
 
     @staticmethod
     def white(msg: str, **kwargs):
         'white, bold, reverse'
-        cprint(text = CLIPPrinter.__adjust_msg__(msg), color = 'white', attrs = ['bold', 'reverse'], **kwargs)
+        cprint(text = CLIPPrinter.__adjust_msg__(msg), color = 'white', attrs = attrs_br, **kwargs)
     
     @staticmethod
     def white_underline(msg: str, **kwargs):
         'white, bold, reverse'
-        cprint(text = CLIPPrinter.__adjust_msg__(msg), color = 'white', attrs = ['bold', 'reverse',' underline'], **kwargs)
+        cprint(text = CLIPPrinter.__adjust_msg__(msg), color = 'white', attrs = attrs_bru, **kwargs)
--- cli_pprinter/colors_terminal_test.py
+++ cli_pprinter/colors_terminal_test.py
@@ -3,3 +3,4 @@ from .colors_terminal import CLIPPrinter
 def test_all():
     assert CLIPPrinter.__adjust_msg__('ha')
     assert not CLIPPrinter.red('ha', end = '')
+    assert not CLIPPrinter.red_underline('ha')
